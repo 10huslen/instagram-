ae62074fcb98185919014530375c28b40c1d9f45
24HP9703 24HP9703 <24HP9703@admins-iMac.local>
2024-12-28 10:43:14 +0800

    2

---
+++ routes/likeRoutes.js
@@ -0,0 +1,20 @@
+const Route = require("express");
+const postModel = require("../models/postSchema");
+
+const likeRoute = Route();
+
+likeRoute.get("/post/like", async (req, res) => {
+const { postId, userId } = req.body;
+try {
+    const likedPostResponse = await postModel.findByIdAndUpdate(postId, {
+        $addSet: {
+            likes: userId,
+        },
+    });
+    res.status(200).json(likedPostResponse);
+} catch (error) {
+    res.status(500).json(error);
+}
+});
+
+module.exports = likeRoute;
